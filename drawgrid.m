274026cf70c0b364d11050f383d73847f1871c10
Brian Wandell <1837145+wandell@users.noreply.github.com>
2025-05-01 15:54:31 -0700

    Update drawgrid.m

--- drawgrid.m
+++ drawgrid.m
@@ -1,30 +1,38 @@
-%%% Draw grid with the specified boxwidth on the image
-function [] = drawgrid(imgfile, boxwidth)
-    img = imread(imgfile);
-    gridimg = img;
-    %%%% If image has only 1 channel, convert to RGB format (3 channels)
-    if length(size(img)) < 3
-        gridimg = cat(3, img, img, img);
-    end
-    [m, n, p] = size(gridimg);
-    %%% Construct (magenta) grid lines
-    for i=1:boxwidth:m
-        for j=1:n
-            for k=1:p
-                gridimg(i,j,k) = 255;
-            end
-            gridimg(i,j,2) = 0;
+function gridimg = drawgrid(imgfile, boxwidth)
+% Draw grid with the specified boxwidth on the image
+%
+% fractal-dimension toolbox
+%
+% See also
+%   getfractaldim (all should be renamed).
+%   Just the two functions should go into utilities on ISETCam
+
+img = imread(imgfile);
+gridimg = img;
+%%%% If image has only 1 channel, convert to RGB format (3 channels)
+if length(size(img)) < 3
+    gridimg = cat(3, img, img, img);
+end
+[m, n, p] = size(gridimg);
+%%% Construct (magenta) grid lines
+for i=1:boxwidth:m
+    for j=1:n
+        for k=1:p
+            gridimg(i,j,k) = 255;
         end
+        gridimg(i,j,2) = 0;
     end
-    for j=1:boxwidth:n
-        for i=1:m
-            for k=1:p
-                gridimg(i,j,k) = 255;
-            end
-            gridimg(i,j,2) = 0;
+end
+for j=1:boxwidth:n
+    for i=1:m
+        for k=1:p
+            gridimg(i,j,k) = 255;
         end
+        gridimg(i,j,2) = 0;
     end
-    imshow(gridimg);
-    %%% Save image, `imgfile` must be a character array
-    imwrite(gridimg, "plots/"+imgfile(5)+"-grid-"+num2str(boxwidth)+".png"); 
+end
+imshow(gridimg);
+
+%%% Save image, `imgfile` must be a character array
+% imwrite(gridimg, "plots/"+imgfile(5)+"-grid-"+num2str(boxwidth)+".png");
 end
